c73badb073112df9fb14aacde9a7a4ac59434dfe
Deborah Goulart <deborah.goulart@crea-rj.org.br>
2025-06-04 17:45:17 -0300

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -29,8 +31,14 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    kotlinOptions {
+        jvmTarget = "11" // <- ADICIONE ISSO
+    }
+
     buildFeatures {
         viewBinding = true
+        compose = true
     }
 }
 
@@ -45,7 +53,18 @@ dependencies {
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
     implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 }
--- app/src/main/java/com/example/taskapp/BaseActivity.java
+++ app/src/main/java/com/example/taskapp/BaseActivity.java
@@ -3,28 +3,25 @@ package com.example.taskapp;
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import com.example.taskapp.databinding.ActivityListaTarefasBinding;
+import com.example.taskapp.databinding.ActivityBaseBinding;
 
-public class ListaTarefas extends AppCompatActivity {
+public class BaseActivity extends AppCompatActivity {
 
-    private ActivityListaTarefasBinding binding;
+    protected ActivityBaseBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityListaTarefasBinding.inflate(getLayoutInflater());
+        binding = ActivityBaseBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        binding.bottomNavigationView.setSelectedItemId(R.id.navigation_dashboard);
 
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
             int itemId = item.getItemId();
-
             if (itemId == R.id.navigation_home) {
                 startActivity(new Intent(this, CadastroForm.class)); //mudar para PaginaUsuario
                 return true;
             } else if (itemId == R.id.navigation_dashboard) {
-                // Já está na tela ListaTarefas, não faz nada
+                startActivity(new Intent(this, ListaTarefas.class));
                 return true;
             } else if (itemId == R.id.navigation_info) {
                 startActivity(new Intent(this, PaginaSobre.class));
@@ -32,11 +29,7 @@ public class ListaTarefas extends AppCompatActivity {
             } else if (itemId == R.id.navigation_pingo) {
                 startActivity(new Intent(this, LoginForm.class)); //mudar para PaginaPingoHome
                 return true;
-            } else if (itemId == R.id.navigation_centro) {
-                // logica de criar tarefa
-                return false;
             }
-
             return false;
         });
     }
