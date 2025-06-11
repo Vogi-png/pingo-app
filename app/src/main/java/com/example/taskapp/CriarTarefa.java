8d14c6d3faff3416c3b5d544ce8ddcaad16ca8cd
gededo <gabrielduartee2005@gmail.com>
2025-06-10 22:48:53 -0300

    Barra de tarefas fix

--- app/src/main/java/com/example/taskapp/CriarTarefa.java
+++ app/src/main/java/com/example/taskapp/CriarTarefa.java
@@ -8,9 +8,6 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.taskapp.databinding.ActivityCriarTarefaBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -36,12 +33,6 @@ public class CriarTarefa extends AppCompatActivity {
         EdgeToEdge.enable(this);
         binding = ActivityCriarTarefaBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         editTitulo = findViewById(R.id.editTitulo);
         editDescricao = findViewById(R.id.editDescricao);
