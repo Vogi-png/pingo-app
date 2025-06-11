5de120fcc6346d785a17a1a955209a35bca3d475
gededo <gabrielduartee2005@gmail.com>
2025-06-10 22:20:51 -0300

    Barra de tarefas fix

--- app/src/main/java/com/example/taskapp/PaginaPlano.java
+++ app/src/main/java/com/example/taskapp/PaginaPlano.java
@@ -6,9 +6,6 @@ import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -21,11 +18,6 @@ public class PaginaPlano extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_pagina_plano);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         //barra de navegacao
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
